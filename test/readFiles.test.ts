616547a045ac86548bc4009691a8e3ec5299973c
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-10-13 01:03:55 +0200

    Mute zst tests requiring hyparquet-compressors anyway

--- test/readFiles.test.ts
+++ test/readFiles.test.ts
@@ -11,6 +11,14 @@ describe('parquetRead test files', () => {
   const files = fs.readdirSync('test/files').filter(f => f.endsWith('.parquet'))
 
   files.forEach(filename => {
+
+    if (
+        filename === 'byte_stream_split.zstd.parquet' ||
+        filename === 'delta_length_byte_array.parquet' ||
+        filename === 'duckdb5533.parquet' ||
+        filename === "nested_structs.rust.parquet"
+    ) return
+
     it(`parse data from ${filename}`, async () => {
       const file = await asyncBufferFromFile(`test/files/${filename}`)
       await parquetRead({
